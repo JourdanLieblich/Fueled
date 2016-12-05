cc1ea08d0d345d1e3e01eb85cef93c5b0933fb2b
JourdanLieblich <jslieblich@gmail.com>
2016-12-04 16:06:06 -0800

    Initial Commit

---
+++ Fueled/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fueled
+//
+//  Created by Jourdan Lieblich on 12/4/16.
+//  Copyright © 2016 Jourdan Lieblich. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
