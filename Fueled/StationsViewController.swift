e96a8aaf81ffd5bfe60fc23c0ed28d667ddcf9dc
JourdanLieblich <jslieblich@gmail.com>
2016-12-06 14:05:40 -0800

    Added Location Services
    Courtesy of NOOR

--- Fueled/StationsViewController.swift
+++ Fueled/StationsViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class StationsViewController: UIViewController {
+class StationsViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.locationManager.delegate  = self
         // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+        
+        self.mapView.showsUserLocation = true
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,22 @@ class StationsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //Mark: - Location Delegate Methods
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error){
+        print("Errors: " + error.localizedDescription)
+    }
     
 }
 
